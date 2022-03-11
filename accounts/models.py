b18c7acc3911fd19b3a019480cf90ddb558c3fc3
kylee <leeky940926@gmail.com>
2022-03-11 20:59:21 +0900

    create accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    
+    class Meta:
+        db_table = 'users'
